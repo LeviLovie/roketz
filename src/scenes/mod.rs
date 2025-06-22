aa9e13d92a146b5b8ab0ff1c7c988c0e6fc5e783
LeviLovie <107021730+LeviLovie@users.noreply.github.com>
2025-06-22 20:39:52 +0700

    Add main menu

--- src/scenes/mod.rs
+++ src/scenes/mod.rs
@@ -10,8 +10,8 @@ use std::sync::{Arc, Mutex};
 use crate::game::{GameData, Scene, SceneManager};
 
 pub fn register(manager: &mut SceneManager, data: Arc<Mutex<GameData>>) -> Result<()> {
-    manager.add_scene(battle::Battle::create(data.clone())?)?;
-    manager.add_scene(menu::Menu::create(data.clone())?)?;
+    manager.add_scene(battle::Battle::create(Some(data.clone()))?)?;
+    manager.add_scene(menu::Menu::create(Some(data.clone()))?)?;
 
     manager.transfer_to("Menu".to_string())?;
     Ok(())
