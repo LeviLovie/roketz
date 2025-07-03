e282a560d4b0db8f65b8d8f0dd5aaca04009ab40
LeviLovie <107021730+LeviLovie@users.noreply.github.com>
2025-07-03 11:07:10 +0300

    Move logs to system dir

--- bin/roketz/src/main.rs
+++ bin/roketz/src/main.rs
@@ -2,8 +2,13 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[macroquad::main("Roketz")]
 async fn main() {
+    let mut log_dir: std::path::PathBuf =
+        dirs::data_local_dir().expect("Failed to get local data directory");
+    log_dir.push("roketz");
+    log_dir.push("logs");
+    std::fs::create_dir_all(&log_dir).expect("Failed to create log directory");
     let file_appender =
-        tracing_appender::rolling::daily("logs", format!("{}.log", env!("CARGO_PKG_NAME")));
+        tracing_appender::rolling::daily(log_dir, format!("{}.log", env!("CARGO_PKG_NAME")));
     let (file_writer, _guard) = tracing_appender::non_blocking(file_appender);
     let file_layer = tracing_subscriber::fmt::layer()
         .with_ansi(false)
