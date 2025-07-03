2a1ba757003ba853f7b37a9aab2573b773a6f379
LeviLovie <107021730+LeviLovie@users.noreply.github.com>
2025-07-03 12:36:54 +0300

    Fix application crash

--- bin/roketz/src/main.rs
+++ bin/roketz/src/main.rs
@@ -2,8 +2,12 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[macroquad::main("Roketz")]
 async fn main() {
+    let log_dir = get_log_dir();
+    if !log_dir.exists() {
+        std::fs::create_dir_all(&log_dir).expect("Could not create log directory");
+    }
     let file_appender =
-        tracing_appender::rolling::daily("logs", format!("{}.log", env!("CARGO_PKG_NAME")));
+        tracing_appender::rolling::daily(log_dir, format!("{}.log", env!("CARGO_PKG_NAME")));
     let (file_writer, _guard) = tracing_appender::non_blocking(file_appender);
     let file_layer = tracing_subscriber::fmt::layer()
         .with_ansi(false)
@@ -25,4 +29,12 @@ async fn main() {
 
     roketz::signals::install_signal_handler().expect("Failed to install signal handler");
     roketz::game::run().await;
+}
+
+fn get_log_dir() -> std::path::PathBuf {
+    if let Some(dir) = dirs::data_local_dir() {
+        dir.join("Roketz").join("logs")
+    } else {
+        std::path::PathBuf::from("logs")
+    }
 }
